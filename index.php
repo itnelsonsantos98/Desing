293921835238ae9d2e70e53efc946fdac24b93b3
Unknown <itnelsonsantos98@gmail.com>
2019-01-25 02:24:21 +0900

    profile

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+	<!-- Parte Menu -->
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="#pro">Profile</a></li>
+			<li><a href="#gal">Galery</a></li>
+			<li><a href="#kon">Kontaktu</a></li>
+		</ul>
+	</div>
+
+	<!-- Taka Menu -->
+
+
+	<!-- Parte Header -->
+	<div class="header">
+		<img src="img/unital2.jpg" width="200px" height="200px">
+		<center><h1>" Bemvindu Mai Hau Nia Web Portofolio "</h></center>	
+		<marquee><p style="margin-top: 80px;font-family:FreeMono;color: red; font-weight: bold;">Universidade Oriental Timor Loro Sa'e (UNITAL)</p></marquee>
+
+	</div>
+	<!-- Taka Header -->
+
+
+	<!-- Parte Profile -->
+	<div class="profile" id="pro">
+		<h2>Profile</h2><hr>
+		<center><img src="img/irmaun.jpg" width="200px" height="200px"></center>
+		<div class="naran">
+			<p>Nre : 170401030</p>
+			<p>Naran Kompletu : Nelson dos Santos</p>
+			<p>Fakuldade : Teknik</p>
+			<p>Departementu : Informatika</p>
+			<p>Semester : III</p>
+
+		</div>
+	</div>
+
+	<!-- Taka profile -->
+	<!-- =================================================================================== -->
+
+	<!-- Parte Galery -->
+
+
+	<div class="galery" id="gal">
+		<center>
+			<h2>Galery</h2><hr>
+		</center>
+		<table border="5">
+			<tr>
+				<td><img src="img/1.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/2.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/3.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/4.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/5.png" width="200px" height="200px" style="border: 2px solid red;"></td>	
+			</tr>
+				<td>imagem 1</td>
+				<td>imagem 2</td>
+				<td>imagem 3</td>
+				<td>imagem 4</td>
+				<td>imagem 5</td>
+			<tr>
+				<td><img src="img/6.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/7.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/8.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/9.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+				<td><img src="img/10.png" width="200px" height="200px" style="border: 2px solid red;"></td>
+			</tr>
+
+			</tr>
+				<td>imagem 1</td>
+				<td>imagem 2</td>
+				<td>imagem 3</td>
+				<td>imagem 4</td>
+				<td>imagem 5</td>
+			<tr>
+
+		</table>
+	</div>
+
+
+	<!-- Taka Galery -->
+	<!-- =========================================================================================== -->
+
+	<!-- Parte Kontaktu -->
+	<div class="kontaktu" id="kon">
+		<center>
+			<h2>Kontaktu</h2><hr>
+		</center>
+		<div class="kontaktu1">
+			<form action="" method="POST">
+				<label for="Naran">Naran Kompletu :</label><br>
+				<input id="input" type="text" name="Naran_Kompletu" id="Naran" placeholder=""><br><br>
+				<label for="Email">Email :</label><br><br>
+				<input id="input" type="email" name="email" id="Email" placeholder=""><br><br>
+				<label for="nu_tlfm">Nu Telefone :</label><br><br>
+				<input id="input" type="number" name="number" id="nu_tlfm" placeholder=""><br><br>
+				<label for="sms">Menssagem :</label><br><br>
+				<textarea id="men" input="text" name="Menssagem" id="sms" rows="8" placeholder=""></textarea><br><br>
+				<input id="si" type="submit" name="Rai" value="Haruka">
+				<input id="si" type="submit" name="Kansela" value="Kansela">
+			</form>
+		</div>
+	</div>
+
+
+
+
+	<!-- Taka Kontaktu -->
+	<!-- =============================================================================== -->
+
+
+	<!-- Parte Footer -->
+	<div class="footer">
+		<p style="color: #fff; text-align: center; margin-top: 30px;">&copy copyright by<a href="http://www.asonsantos.net" target="blank" style="text-decoration: none; color: blue;"> ASONSANTOS.NET </a></p>
+	</div>
+
+	<!-- Taka Footer -->
+</body>
+</html> 
